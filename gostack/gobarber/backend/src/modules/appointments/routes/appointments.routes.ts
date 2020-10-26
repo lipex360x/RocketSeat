4978d0be3cae78fa4d3462c1ab7a4fea9735345d
Felipe Borges <lipex360@gmail.com>
2020-10-26 17:06:37 -0300

    Validate Date

--- gostack/gobarber/backend/src/modules/appointments/routes/appointments.routes.ts
+++ gostack/gobarber/backend/src/modules/appointments/routes/appointments.routes.ts
@@ -1,17 +1,29 @@
 import { Router } from 'express'
 import { v4 as uuid } from 'uuid'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
 
 const router = Router()
 
-const appointments = []
+interface AppointmentProps {
+  id: string
+  provider: string
+  date: Date
+}
+
+const appointments: AppointmentProps[] = []
 
 router.post('/', (request, response) => {
   const { provider, date } = request.body
 
+  const parsedDate = startOfHour(parseISO(date))
+  const findAppointment = appointments.find(appointment => isEqual(parsedDate, appointment.date))
+
+  if (findAppointment) return response.status(400).json({ message: 'This appointment is already booked' })
+
   const appointment = {
     id: uuid(),
     provider,
-    date
+    date: parsedDate
   }
 
   appointments.push(appointment)
