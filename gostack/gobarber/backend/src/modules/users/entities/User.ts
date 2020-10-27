7f884156367970374c22d99d6f0fe6b2628f85c9
Felipe Borges <lipex360@gmail.com>
2020-10-27 16:05:33 -0300

    Entity User

---
+++ gostack/gobarber/backend/src/modules/users/entities/User.ts
@@ -0,0 +1,28 @@
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('users')
+export default class User {
+  @PrimaryColumn()
+  id?: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  @BeforeInsert()
+  userId () {
+    this.id = uuid()
+  }
+}
