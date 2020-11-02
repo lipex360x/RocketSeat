1e60ba2177c90ff0e2d1c42990d32a0b7d463ed9
Felipe Borges <lipex360@gmail.com>
2020-11-02 18:30:09 -0300

    Appointments Routes and Controllers refactory

--- gostack/gobarber/backend/src/modules/appointments/routes/providers.routes.ts
+++ gostack/gobarber/backend/src/modules/appointments/routes/providers.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express'
 
 import sessionStarted from '@shared/middlewares/sessions/sessionStarted'
 import ListProvidersController from '../controllers/ListProvidersController'
+import ListDayAvailabilityController from '../controllers/ListDayAvailabilityController'
+import ListMonthAvailabilityController from '../controllers/ListMonthAvailabilityController'
 
 const listProvidersController = new ListProvidersController()
+const listDayAvailabilityController = new ListDayAvailabilityController()
+const listMonthAvailabilityController = new ListMonthAvailabilityController()
 
 const router = Router()
 
 router.use(sessionStarted)
 router.get('/', listProvidersController.index)
+router.get('/:provider_id/month-availability', listMonthAvailabilityController.index)
+router.get('/:provider_id/day-availability', listDayAvailabilityController.index)
 
 export default router
