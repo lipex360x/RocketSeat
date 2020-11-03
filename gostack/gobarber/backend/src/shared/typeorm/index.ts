27728ba70b680ba0b3ac133588842c0c9c380f2a
Felipe Borges <lipex360@gmail.com>
2020-11-03 10:30:14 -0300

    MongoDB Install

--- gostack/gobarber/backend/src/shared/typeorm/index.ts
+++ gostack/gobarber/backend/src/shared/typeorm/index.ts
@@ -1,13 +1,15 @@
-import { createConnection } from 'typeorm'
+import { createConnections } from 'typeorm'
 
 const connectDB = async () => {
   try {
-    const connect = await createConnection()
+    const connect = await createConnections()
 
-    console.log(`💖 Connected to BD ${connect.options.database}`)
+    console.log(`💖 Connected to Postgres ${connect[0].options.database}`)
+    console.log(`💖 Connected to MongoDB ${connect[1].options.database}`)
 
     process.on('SIGINT', () => {
-      connect.close().then(() => console.log(`  💔 Disconnected to BD ${connect.options.database}`))
+      connect[0].close().then(() => console.log(`  💔 Disconnected to BD ${connect[0].options.database}`))
+      connect[1].close().then(() => console.log(`  💔 Disconnected to MongoDB ${connect[1].options.database}`))
     })
   } catch (error) {
     console.log('Database Error: ', error.message)
