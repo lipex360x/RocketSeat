97267988541cfaacd567b00ad6b791428b517ef9
Felipe Borges <lipex360@gmail.com>
2020-11-03 16:52:56 -0300

    Cache Redis Config

--- gostack/gobarber/backend/src/modules/appointments/services/ListAppointments/ListAppointmentsService.ts
+++ gostack/gobarber/backend/src/modules/appointments/services/ListAppointments/ListAppointmentsService.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import { injectable, inject } from 'tsyringe'
 
 import IAppointmentsRepository from '@modules/appointments/repositories/interfaces/IAppointmentsRepository'
+import ICacheProvider from '@shared/container/providers/CacheProvider/interfaces/ICacheProvider'
 import Appointment from '@modules/appointments/entities/Appointment'
 
 interface Request {
@@ -15,11 +16,19 @@ interface Request {
 export default class ListAppointmentsService {
   constructor (
     @inject('AppointmentsRepository')
-    private repository: IAppointmentsRepository
+    private repository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cache: ICacheProvider
   ) {}
 
   async execute ({ provider_id, year, month, day }:Request): Promise<Appointment[]> {
+    const cacheData = await this.cache.getCache({ key: 'testeCache' })
+    console.log(cacheData)
+
     const appointments = await this.repository.findAllInDay({ provider_id, year, month, day })
+
+    // this.cache.saveCache({ key: 'testeCache', value: 'cache saved' })
 
     return appointments
   }
--- gostack/gobarber/backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ gostack/gobarber/backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -1,19 +1,23 @@
 import Redis, { Redis as RedisClient } from 'ioredis'
 import ICacheProvider, { DeleteCacheProps, GetCacheProps, SaveCacheProps, TruncateAllProps } from '../interfaces/ICacheProvider'
 
+import cacheConfig from '../config/cache.config'
+
 export default class RedisCacheProvider implements ICacheProvider {
   private client: RedisClient
 
   constructor () {
-    this.client = new Redis()
+    this.client = new Redis(cacheConfig.config.redis)
   }
 
   async saveCache ({ key, value }:SaveCacheProps): Promise<void> {
-
+    await this.client.set(key, JSON.stringify(value))
   }
 
   async getCache ({ key }:GetCacheProps): Promise<string> {
-    return ''
+    const data = await this.client.get(key)
+
+    return data
   }
 
   async deleteCache ({ key }:DeleteCacheProps): Promise<void> {
