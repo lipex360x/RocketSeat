5c12ce9eb423dd913c40a0aab0a549a4cd52d911
Felipe Borges <felipe.bambirra@tcsindustrial.com.br>
2020-11-04 17:27:50 -0200

    Migration and Customer Entity

--- desafios/nivel_04/typeorm_relations/src/modules/customers/infra/typeorm/entities/Customer.ts
+++ desafios/nivel_04/typeorm_relations/src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -1,21 +1,34 @@
+import {v4 as uuid} from 'uuid'
 import {
   Entity,
   Column,
-  PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeInsert,
+  PrimaryColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  @BeforeInsert()
+  setProps(){
+    this.id = uuid()
+  }
 }
 
 export default Customer;
