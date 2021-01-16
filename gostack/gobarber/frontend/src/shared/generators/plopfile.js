731529eb28db129e55850870343ffee9c6e25309
Felipe Borges <lipex360@gmail.com>
2021-01-16 18:06:12 -0300

    PlopJS added

---
+++ gostack/gobarber/frontend/src/shared/generators/plopfile.js
@@ -0,0 +1,23 @@
+module.exports = function (plop) {
+  // controller generator
+  plop.setGenerator('module', {
+    description: 'Create a Module',
+    prompts: [{
+      type: 'input',
+      name: 'name',
+      message: 'Module name please'
+    }],
+    actions: [
+      {
+        type: 'add',
+        path: '../../components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.hbs'
+      }
+    ]
+  })
+}
