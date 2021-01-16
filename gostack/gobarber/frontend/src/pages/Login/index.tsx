8c0ffb972e925e5e179ced6e644bb023bdf862e9
Felipe Borges <lipex360@gmail.com>
2021-01-16 18:16:23 -0300

    Routes Added

---
+++ gostack/gobarber/frontend/src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+const Login = () => {
+  return (
+    <>
+      <S.Header>Hello Login</S.Header>
+    </>
+  )
+}
+
+export default Login
